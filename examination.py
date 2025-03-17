907ed76f68627ab55b3b35e0ea46a99d5b9021b7
Huang-xiaoyi <75195147+Huang-xiaoyi@users.noreply.github.com>
2025-03-17 11:25:51 +0800

    Add files via upload

--- examination.py
+++ examination.py
@@ -1,70 +1,81 @@
-import os
+import os,re
 
-def sum(sc):
-   return sc[0]+sc[1]
-
-def isIn():    #查找学生
-   search = input('找谁？')
-   if search in score:
-      print(score[search])
-   else:
+def isIn(search = ''):    #查找学生
+   if search == '':
+      search = input('找谁？')
+   with open('Student.txt', 'r') as f:
+      s = f.readline()
+      while s != '':
+         G = re.match(r'(\w+) (\d+) (\d+)', s)
+         if G.group(1) == search:
+            print(G.group(0))
+            return True
+         else:
+            s = f.readline()
       print('没这个人')
+      return False
 
 def add_Student():
-   L = []
    name = input('名字：')
-   chinese = int(input('语文成绩：'))
-   Smath = int(input('数学成绩'))
-   if score.get(name):
+   chinese = input('语文成绩：')
+   Smath = input('数学成绩')
+   if isIn(name):
       print('已经有这么个人了')
    else:
-      student.append(name)
-      score[name] = [chinese, Smath]
-      print('添加成功')
+      print("开始插入新数据：\n")
+      with open('Student.txt', 'a', encoding= 'UTF-8') as f:
+         s = name + ' ' + chinese + ' ' + Smath + '\n'
+         f.write(s)
 
-def down_sort(s):     #  升序
-   d_score = sorted(score, key = lambda x : sum(score[x]) )
-   for x in range(len(s)):
-        if d_score[x] in score:
-            total = sum(score[d_score[x]])
-            print(d_score[x],score[d_score[x]][0],score[d_score[x]][1], total)
+def student_sort(isReverse):
+   with open('Student.txt', 'r') as f:
+      lines = f.readlines()
+   
+   student = []
+   for line in lines:
+      part = line.strip().split()
+      name = part[0]
+      Smath = int(part[1])
+      chinese = int(part[2])
+      total = Smath + chinese
+      student.append((name, Smath, chinese, total))
 
+   sorted_student = sorted(student, key=lambda x : x[3], reverse=isReverse)
 
-def up_sort(s):         #  降序
-   u_score = sorted(score, key = lambda x : sum(score[x]), reverse=True )
-   for x in range(len(s)):
-        if u_score[x] in score:
-            total = sum(score[u_score[x]])
-            print(u_score[x],score[u_score[x]][0],score[u_score[x]][1], total)
+   for student in sorted_student:
+      print(f"{student[0]} {student[1]} {student[2]} {student[3]}")
+
 
 def Select_All():
-    print('名字  语文  数学  总分')
-    for x in range(len(student)):
-        if student[x] in score:
-            total = sum(score[student[x]])
-            print(student[x],score[student[x]][0],score[student[x]][1], total)
-    while True:
-        sub_choose = input('''
-           \033[33m1.按总分降序排序
-            2.按总分升序排序
-            0.返回上一级\n\033[0m''')   #改变字体颜色，用于区分菜单和学生成绩信息
+   print('名字  语文  数学  总分')
+   with open('Student.txt', 'r') as f:
+      s = f.readline()
+      while s != '':
+         m = re.match(r'(\w+) (\d+) (\d+)', s)
+         s = f.readline()
+         total = int(m.group(2)) + int(m.group(3))
+         print(m.group(0) + ' %d' % (total))
 
-        sub_choose = int(sub_choose)
-
-        match sub_choose:
-            case 1:
-               os.system('cls')
-               up_sort(student)
-            case 2:
-               os.system('cls')
-               down_sort(student)
-            case 0:
-               break
-    return
+   while True:
+      sub_choose = input('''
+         \033[33m1.按总分降序排序
+         2.按总分升序排序
+         0.返回上一级\n\033[0m''')   #改变字体颜色，用于区分菜单和学生成绩信息
 
 
-score = {'a1': [77, 68], 'a2': [79, 89], 'a3': [88, 66]}   #预设学生信息
-student = ['a1', 'a2', 'a3']    #为字典服务的学生名单
+      match sub_choose:
+         case '1':
+            os.system('cls')
+            student_sort(True)
+         case '2':
+            os.system('cls')
+            student_sort(False)
+         case '0':
+            break
+   return
+
+
+
 while True:
     first_choose = input('''
      1.查看全班成绩
@@ -72,17 +83,17 @@ while True:
      3.添加学生
      0.退出\n''')
 
-    first_choose = int(first_choose)
+   
 
     match first_choose:
-        case 1:
+        case '1':
            os.system('cls')
            Select_All()
-        case 2:
+        case '2':
            os.system('cls')
            isIn()
-        case 3:
+        case '3':
            os.system('cls')
            add_Student()
-        case 0:
+        case '0':
            break
